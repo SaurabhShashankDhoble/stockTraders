907e815ec75eddefe53d993d9acef1e7e1e47d9a
Saurabh Dhoble <dhoblesaurabh35@gmail.com>
2021-04-17 18:10:21 +0530

    a

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+app.get("/",function(req, res){
+  res.render("index",);
+});
+app.listen(process.env.PORT || 3000,function(req,res){
+  console.log("server is running...");
+});
