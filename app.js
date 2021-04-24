2b3e90d090db535945036046925ba446dec4d95e
Saurabh Dhoble <dhoblesaurabh35@gmail.com>
2021-04-24 18:24:24 +0530

    made free docs page

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 app.get("/",function(req, res){
-  res.render("index",);
+  res.render("index");
 });
+
+app.get("/freeContent",function(req, res){
+  res.render("free");
+});
+app.get("/premiumContent",function(req, res){
+  res.render("premium");
+});
+
 app.listen(process.env.PORT || 3000,function(req,res){
   console.log("server is running...");
 });
